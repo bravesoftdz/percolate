d79752486ac940c912b6987678fff16984ff4fc8
Gil Oliveira <giloliveira@outlook.com>
2016-05-10 09:58:30 +0100

    Acrescentei todo o tipo de botões

--- unit1.pas
+++ unit1.pas
@@ -5,10 +5,18 @@ unit Unit1;
 interface
 
 uses
-  Classes, SysUtils, FileUtil, Forms, Controls, Graphics, Dialogs;
+  Classes, SysUtils, FileUtil, Forms, Controls, Graphics, Dialogs, ExtCtrls,
+  StdCtrls;
 
 type
+
+  { TForm1 }
+
   TForm1 = class(TForm)
+    Image1: TImage;
+    Image2: TImage;
+    Image3: TImage;
+    procedure Image1Click(Sender: TObject);
   private
     { private declarations }
   public
@@ -21,6 +29,13 @@ var
 implementation
 
 {$R *.lfm}
+
+{ TForm1 }
+
+procedure TForm1.Image1Click(Sender: TObject);
+begin
+
+end;
 
 end.
 

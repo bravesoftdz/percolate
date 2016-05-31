1bb64a25d3a8dddb5933dfa982ca28c083c2ed9d
LusitanianDev <cesarmcsousa@gmail.com>
2016-05-31 10:04:18 +0100

    Beta 1.0

--- unit1.pas
+++ unit1.pas
@@ -6,7 +6,7 @@ interface
 
 uses
   Classes, SysUtils, FileUtil, Forms, Controls, Graphics, Dialogs, ExtCtrls,
-  StdCtrls, Unit2, Unit3;
+  StdCtrls,Unit3, lclintf;
 
 type
 
@@ -17,11 +17,13 @@ type
     Image11: TImage;
     Image2: TImage;
     Image3: TImage;
+    Image4: TImage;
     Label1: TLabel;
     Label2: TLabel;
     procedure Image11Click(Sender: TObject);
     procedure Image1Click(Sender: TObject);
     procedure Image2Click(Sender: TObject);
+    procedure Image4Click(Sender: TObject);
   private
     { private declarations }
   public
@@ -51,6 +53,11 @@ procedure TForm1.Image2Click(Sender: TObject);
 begin
   Form3.Show;
   Form1.Hide;
+end;
+
+procedure TForm1.Image4Click(Sender: TObject);
+begin
+  OpenURL('https://github.com/genericstudios/percolate/wiki/How-to-play');
 end;
 
 end.
